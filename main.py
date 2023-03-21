e78227b75483020be7a1c27e2985222fa3c905ab
amat <saeram00.nova@gmail.com>
2023-03-21 15:09:49 -0300

    Fixed API
        - Edited fields in pandas queries, so they now match columns in
          movies_db.csv file.

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ app = FastAPI(title="Pelis y Series")
 async def read_root():
      return "Base de datos de películas dispobibles en distintas plataformas"
 
-@app.get("/get_max_duration/{year}_{platform}_{duration_type}")
+@app.get("/get_max_duration/")
 async def get_max_duration(year: int, platform: str, duration_type: str):
     df = pd.read_csv("movies_db.csv")
     # Filter columns
     filtered_df = df.loc[
-        (df["year"] == year if year else True) &
+        (df["release_year"] == year if year else True) &
         (df["platform"] == platform if platform else True) &
         (df["duration_type"] == duration_type if duration_type else True)
     ]
@@ -22,20 +22,20 @@ async def get_max_duration(year: int, platform: str, duration_type: str):
     return max_duration_movie["title"]
 
 
-@app.get("/get_score_count/{platform}_{scored}_{year}")
+@app.get("/get_score_count/")
 def get_score_count(platform: str, scored: float, year: int) -> int:
     df = pd.read_csv("movies_db.csv")
     
     # Filter DataFrame by platform and year
-    filtered_df = df.loc[(df["platform"] == platform) & (df["year"] == year)]
+    filtered_df = df.loc[(df["platform"] == platform) & (df["release_year"] == year)]
 
     # Count movies with score higher than "scored" param
-    movie_count = filtered_df.loc[filtered_df["score"] > scored].groupby("platform")["title"].count()[platform]
+    movie_count = filtered_df.loc[filtered_df["score_avg"] > scored].groupby("platform")["title"].count()[platform]
 
     return movie_count
 
 
-@app.get("/get_count_platform/{platform}")
+@app.get("/get_count_platform/")
 async def get_count_platform(platform: str) -> int:
     df = pd.read_csv("movies_db.csv")
     
@@ -48,13 +48,13 @@ async def get_count_platform(platform: str) -> int:
     return quantity_by_platform
 
 
-@app.get("/get_actor/{platform}_{year}")
+@app.get("/get_actor/")
 async def get_actor(platform: str, year: int):
 
     df = pd.read_csv("movies_db.csv")
 
     # Filter DataFrame by platform and year
-    filtered_df = df[(df["platform"] == platform) & (df["year"] == year)]
+    filtered_df = df[(df["platform"] == platform) & (df["release_year"] == year)]
 
     # Split actors string into a list and convert to DataFrame
     actors_df = filtered_df["cast"].str.split(", ", expand=True)
