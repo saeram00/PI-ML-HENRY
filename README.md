3a4456903c150905ad65889fb07347f14766e738
amat <saeram00.nova@gmail.com>
2023-04-13 17:16:18 -0400

    Progressed API
        - Added two required functions to app/main.py file.
        - Provided a simple Dockerfile (WIP).
        - Deleted data folder and provided a link to it in README.md.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PI-ML-HENRY
+
 Individual project related to Machine Learning
+
+#### Link to data files folder: [Drive](https://drive.google.com/drive/folders/1PftPdEGvb-byhq2BYr1cB5KJQcmJhZeW?usp=sharing)
+
+Inside the data folder you'll find 2 gzipped tar files: raw_data.tgz and processed_data.tgz. The first, "raw", contains a data folder with the datasets provided by HENRY for the project before any transformations were applied to them, while the second, "processed", is the result of all the ETL operations contained in ETL/etl.ipynb applied to the raw csv files, resulting in 2 simple csv files: movies_db.csv and processed_ratings.csv, both of which were used throughout the rest of the project.
+
+The setup.sh script provided in this repo should allow you to download either or both of them at your discretion.
---
+++ app/main.py
@@ -0,0 +1,128 @@
+from fastapi import FastAPI
+import pandas as pd
+
+from pathlib import Path
+
+
+MOVIE_CSV_PATH = Path(__file__).parents[1].joinpath("data", "movies_db.csv").resolve()
+app = FastAPI(title="Pelis y Series")
+
+@app.get("/")
+async def read_root():
+     return "Base de datos de películas dispobibles en distintas plataformas"
+
+@app.get("/get_max_duration/")
+async def get_max_duration(year: int, platform: str, duration_type: str) -> dict:
+    df = pd.read_csv(MOVIE_CSV_PATH)
+
+    # Filter columns
+    filtered_df = df.loc[
+        (df["release_year"] == year if year else True)
+        & (df["platform"] == platform.lower() if platform else True)
+        & (df["duration_type"] == duration_type.lower() if duration_type else True)
+    ]
+
+    # Get movie with max duration
+    max_duration_movie = filtered_df.loc[filtered_df["duration_int"].idxmax()]["title"]
+    return {"pelicula": max_duration_movie}
+
+
+@app.get("/get_score_count/")
+async def get_score_count(platform: str, scored: float, year: int) -> dict:
+    df = pd.read_csv(MOVIE_CSV_PATH)
+    
+    # Filter DataFrame by platform and year
+    filtered_df = df.loc[
+        (df["platform"] == platform.lower())
+        & (df["release_year"] == year)
+    ]
+
+    # Count movies with score higher than "scored" param
+    movie_count = filtered_df.loc[
+        filtered_df["score_avg"] > scored
+    ].groupby("platform")["id"].count()[platform.lower()].item()
+
+    return {
+        "plataforma": platform.title(),
+        "cantidad": movie_count,
+        "anio": year,
+        "score": scored
+    }
+
+
+@app.get("/get_count_platform/")
+async def get_count_platform(platform: str) -> dict:
+    df = pd.read_csv(MOVIE_CSV_PATH)
+    
+    # Filter DataFrame by platform
+    filtered_df = df.loc[df["platform"] == platform.lower()]
+
+    # Count movies by platform
+    quantity_by_platform = filtered_df.groupby("platform")["id"].count()[platform.lower()].item()
+
+    return {
+        "plataforma": platform.title(),
+        "peliculas": quantity_by_platform
+    }
+
+
+@app.get("/get_actor/")
+async def get_actor(platform: str, year: int) -> dict:
+    df = pd.read_csv(MOVIE_CSV_PATH)
+
+    # Filter DataFrame by platform and year
+    filtered_df = df.loc[
+        (df["platform"] == platform.lower())
+        & (df["release_year"] == year)
+    ]
+
+    # Split actors string into a list and convert to DataFrame
+    actors_df = filtered_df["cast"].str.split(", ", expand=True)
+
+    # Obtain most repeated actor
+    actor_count = actors_df.stack().value_counts().reset_index()
+    actor_repeated = actor_count.iloc[0]["index"]
+    actor_apparitions = actor_count.iloc[0,1].item()
+
+    return {
+        "plataforma": platform.title(),
+        "anio": year,
+        "actor": actor_repeated.title(),
+        "apariciones": actor_apparitions
+    }
+
+
+@app.get("/prod_per_country/")
+async def prod_per_country(prod_type: str, country: str, year: int) -> dict:
+    df = pd.read_csv(MOVIE_CSV_PATH)
+
+    # Filter columns
+    filtered_df = df.loc[
+        (df["country"] == country.lower())
+        & (df["release_year"] == year)
+    ]
+
+    # Group by product type: either movie or tv show, and get the total sum
+    product_quantity = filtered_df.groupby("type")["id"].count()[prod_type.lower()].item()
+
+    return {
+        "pais": country.title(),
+        "anio": year,
+        "peliculas": product_quantity
+    }
+
+
+@app.get("/get_contents/")
+async def get_contents(rating: str) -> dict:
+    df = pd.read_csv(MOVIE_CSV_PATH)
+    
+    # Filter columns
+    filtered_df = df.loc[df["rating"] == rating.lower()]
+
+    # Group by type and count the total sum of elements
+    total_elements = filtered_df.groupby("type")["id"].count().sum().item()
+
+    return {
+        "rating": rating.upper(),
+        "contenido": total_elements
+    }
